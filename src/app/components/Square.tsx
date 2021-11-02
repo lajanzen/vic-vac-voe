43234d6bd96201e5ff87da92ce85a8d5ce5de26d
lajanzen <lara-janzen@hotmail.com>
2021-11-02 17:31:20 +0100

    Make game work

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+type squareProps = {
+  onClick: () => void;
+  value: string;
+};
+
+function Square(props: squareProps): JSX.Element {
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        props.onClick();
+      }}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
